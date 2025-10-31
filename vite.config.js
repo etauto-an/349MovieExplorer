1cc1cdf51931442523ba73cec30fea0da0b951bb
etauto-an <etauto-an@csu.fullerton.edu>
2025-10-31 12:25:32 -0700

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// **IMPORTANT: Replace 'MovieExplorer' with your actual repository name**
+const repoName = '/349MovieExplorer/'; 
+
 export default defineConfig({
   plugins: [react()],
-})
+  // Sets the base public path when serving and building the application
+  base: repoName, 
+});
